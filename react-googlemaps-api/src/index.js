8e4c79a62de289f802743b3ce0af1a2e30d8c00d
Vanessa Park <bohyunparkv@gmail.com>
2023-08-23 21:58:19 +0100

    Applying Google Maps API into the project

---
+++ react-googlemaps-api/src/index.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import { useMemo } from "react";
+// import { GoogleMap }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default Home = () => {
+  const {} = useLoadScript({
+    googleMapsApiKey:
+  })
+
+  return <div>Map</div>
+}
