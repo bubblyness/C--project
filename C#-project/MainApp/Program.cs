90a657d6d84dea6fadacca0961b5a972a74e75ec
vanessa <bohyunparkv@gmail.com>
2024-06-02 15:35:35 +0900

    List T and Collections of Data

--- C#-project/MainApp/Program.cs
+++ C#-project/MainApp/Program.cs
@@ -1,52 +1,67 @@
-﻿using System;
+﻿// using System;
 
-namespace IntegerLiterals
+// namespace IntegerLiterals
+// {
+//     class MainApp
+//     {
+//         static void Main(string[] args)
+//         // {
+//         //     byte a = 240;
+//         //     Console.WriteLine($"a={a}");
+
+//         //     byte b = 0b1111_0000;
+//         //     Console.WriteLine($"b={b}");
+
+//         //     byte c = 0XF0;
+//         //     Console.WriteLine($"c={c}");
+
+//         //     uint d = 0x1234_abcd;
+//         //     Console.WriteLine($"d={d}");
+//         // }
+//         // {
+//         //     sbyte a = -10;
+//         //     byte b = 40;
+//         //     Console.WriteLine($"a={a}, b={b}");
+
+//         //     short c = -30000;
+//         //     ushort d = 60000;
+//         //     Console.WriteLine($"c={c}, d={d}");
+
+//         //     int e = -1000_000;
+//         //     uint f = 3_0000_0000;
+//         //     Console.WriteLine($"e={e}, f={f}");
+
+//         //     long g = -5000_0000_0000;
+//         //     ulong h = 200_0000_0000_0000_0000;
+//         //     Console.WriteLine($"g={g}, h={h}");
+//         // }
+//         // {
+//         //     byte a = 240; // decimal literal
+//         //     Console.WriteLine($"a={a}");
+
+//         //     byte b = 0b1111_0000; // binary literal
+//         //     Console.WriteLine($"b={b}");
+
+//         //     byte c = 0XF0; // hexadecimal literal
+//         //     Console.WriteLine($"c={c}");
+
+//         //     uint d = 0x1234_abcd; //hexadecimal literal
+//         //     Console.WriteLine($"d={d}");
+//         // }
+
+
+// }
+// }
+
+using Microsoft.VisualBasic;
+
+var names = new List<string> { "Jiwon", "Bori", "Katarina" };
+
+names.Add("Amelia");
+names.Add("Eleanor");
+
+foreach (var name in names)
 {
-    class MainApp
-    {
-        static void Main(string[] args)
-        // {
-        //     byte a = 240;
-        //     Console.WriteLine($"a={a}");
-
-        //     byte b = 0b1111_0000;
-        //     Console.WriteLine($"b={b}");
-
-        //     byte c = 0XF0;
-        //     Console.WriteLine($"c={c}");
-
-        //     uint d = 0x1234_abcd;
-        //     Console.WriteLine($"d={d}");
-        // }
-        // {
-        //     sbyte a = -10;
-        //     byte b = 40;
-        //     Console.WriteLine($"a={a}, b={b}");
-
-        //     short c = -30000;
-        //     ushort d = 60000;
-        //     Console.WriteLine($"c={c}, d={d}");
-
-        //     int e = -1000_000;
-        //     uint f = 3_0000_0000;
-        //     Console.WriteLine($"e={e}, f={f}");
-
-        //     long g = -5000_0000_0000;
-        //     ulong h = 200_0000_0000_0000_0000;
-        //     Console.WriteLine($"g={g}, h={h}");
-        // }
-        {
-            byte a = 240;
-            Console.WriteLine($"a={a}");
-
-            byte b = 0b111_0000;
-            Console.WriteLine($"b={b}");
-
-            byte c = 0XF0;
-            Console.WriteLine($"c={c}");
-
-            uint d = 0x1234_abcd;
-            Console.WriteLine($"d={d}");
-        }
-    }
+    Console.WriteLine($"Hey {name.ToUpper()}!!");
 }
+
